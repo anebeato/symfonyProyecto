45b5ad8d6c15883cc94d0dc9372464fd44a79199
Abi-2004 <104345097+Abi-2004@users.noreply.github.com>
2024-10-16 13:26:31 +0200

    Implement `PasswordAuthenticatedUserInterface` in `Usuario` entity
    * Add `use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface` import
    * Implement `PasswordAuthenticatedUserInterface` in `Usuario` class declaration

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -7,9 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
-class Usuario
+class Usuario implements PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
